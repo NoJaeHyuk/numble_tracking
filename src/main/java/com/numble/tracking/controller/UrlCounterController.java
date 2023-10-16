df42720133727d791b858da41c7b6397e135414d
jaehyuk <jaehyuk7722@naver.com>
2023-10-16 22:23:22 +0900

    docs: Swagger API 명세 작성
    - API에대한 Swagger 명세 초기 설정

--- src/main/java/com/numble/tracking/controller/UrlCounterController.java
+++ src/main/java/com/numble/tracking/controller/UrlCounterController.java
@@ -27,13 +27,25 @@ public class UrlCounterController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @ApiOperation(value = "URI 조회수 조회", notes = "호출 시 오늘 조회수와 누적 조회수를 재공한다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK !!"),
+        @ApiResponse(code = 500, message = "Internal Server Error !!"),
+        @ApiResponse(code = 404, message = "Not Found !!")
+    })
     @GetMapping("/stats")
-    public ResponseEntity<?> getStats(@RequestParam String url){
+    public ResponseEntity<?> getStats(@ApiParam(value = "URL", required = true) @RequestParam String url){
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @ApiOperation(value = "7일간의 일간 조회수 조회", notes = "호출 시 7일간의 조회수를 제공한다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK !!"),
+        @ApiResponse(code = 500, message = "Internal Server Error !!"),
+        @ApiResponse(code = 404, message = "Not Found !!")
+    })
     @GetMapping("/stats/weekly")
-    public ResponseEntity<?> getWeeklyStats(@RequestParam String url){
+    public ResponseEntity<?> getWeeklyStats(@ApiParam(value = "URL", required = true) @RequestParam String url){
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
