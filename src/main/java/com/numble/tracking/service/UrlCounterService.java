90b9d4b867870672ba80995f14ce3ac3eae1432e
jaehyuk <jaehyuk7722@naver.com>
2023-10-17 16:19:20 +0900

    refactor: 코드 정리
    - 불필요한 코드 정리

--- src/main/java/com/numble/tracking/service/UrlCounterService.java
+++ src/main/java/com/numble/tracking/service/UrlCounterService.java
@@ -1,18 +1,10 @@
 package com.numble.tracking.service;
 
-import com.numble.tracking.domain.UrlCounter;
-import com.numble.tracking.repository.UrlCounterRepository;
-import java.time.LocalDate;
-import javax.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UrlCounterService {
 
-    @Transactional
     public void increaseCounter(String url);
 
 }
