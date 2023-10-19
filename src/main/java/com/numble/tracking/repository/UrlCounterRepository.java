8eac4278fa4569a7216ac0e2a0460483ac29208b
jaehyuk <jaehyuk7722@naver.com>
2023-10-19 14:49:02 +0900

    fix: 동시성 테스트 시 중복 Insert 이슈 수정
    - 테이블 unique 설정 추가로 중복 방지
    - Optimistic Lock을 이용해 insert 중복 시 재시도 로직 구현

--- src/main/java/com/numble/tracking/repository/UrlCounterRepository.java
+++ src/main/java/com/numble/tracking/repository/UrlCounterRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,8 +15,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UrlCounterRepository extends JpaRepository<UrlCounter, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    //@Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(LockModeType.OPTIMISTIC)
     Optional<UrlCounter> findByUrlAndDate(String url, LocalDate date);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "update UrlCounter u set u.count = u.count + 1 where u.id = :urlCountId")
+    void increaseUrlCount(Long urlCountId);
 
     @Query("SELECT u.count FROM UrlCounter u WHERE u.url = :url AND u.date = :date")
     Long findUrlCounter(@Param("url") String url, @Param("date") LocalDate date);
--- src/test/java/com/numble/tracking/service/UrlCounterServiceConcurrencyTest.java
+++ src/test/java/com/numble/tracking/service/UrlCounterServiceConcurrencyTest.java
@@ -3,6 +3,8 @@ package com.numble.tracking.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.numble.tracking.common.exception.CustomException;
+import com.numble.tracking.domain.UrlCounter;
+import com.numble.tracking.facade.OptimisticLockStockFacade;
 import com.numble.tracking.repository.UrlCounterRepository;
 import java.time.LocalDate;
 import java.util.concurrent.CountDownLatch;
@@ -10,6 +12,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +26,14 @@ class UrlCounterServiceConcurrencyTest {
     private UrlCounterRepository urlCounterRepository;
 
     @Autowired
-    private UrlCounterService urlCounterService;
+    private OptimisticLockStockFacade urlCounterService;
+
+    //@BeforeEach
+    public void insert(){
+        UrlCounter urlCounter = new UrlCounter("https://jh7722.test.com/",LocalDate.now());
+
+        urlCounterRepository.saveAndFlush(urlCounter);
+    }
 
     @AfterEach
     public void delete() {
@@ -34,8 +44,8 @@ class UrlCounterServiceConcurrencyTest {
     @DisplayName("동시에 100명이 특정 URL을 조회")
     void increaseCounterConcurrency() throws InterruptedException {
         AtomicInteger successCount = new AtomicInteger();
-        int threadCount = 5;
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        int threadCount = 50;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
         CountDownLatch latch = new CountDownLatch(threadCount);
 
         for (int i = 0; i < threadCount; i++) {
@@ -59,7 +69,8 @@ class UrlCounterServiceConcurrencyTest {
         Long urlCounter = urlCounterRepository.findUrlCounter("https://jh7722.test.com/",
             LocalDate.now());
 
-        assertThat(urlCounter).isEqualTo(100);
+        assertThat(successCount.get()).isEqualTo(threadCount);
+        assertThat(urlCounter).isEqualTo(50);
     }
 
 }
